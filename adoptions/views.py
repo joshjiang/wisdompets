f433bc4b3ffaf59270eec1ff17fcf75d24d2f4cb
Josh Jiang <josh.jiang.dbf@gmail.com>
2018-06-03 14:10:23 -0400

    Wrote regex for home and pet detail URLS, added views for home and pet detail

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<p>Home View</p>')
+
+def pet_detail(request, id):
+    return HttpResponse('<p>Pet Detail View with the ID {}</p'.format(id))
+
