75090b5e6aa3c311c7fa49d0ea41f58b78cd4e61
Josh Jiang <josh.jiang.dbf@gmail.com>
2018-06-03 18:09:39 -0400

    Added static directory and templates for details view and home

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from .models import Pet
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<p>Home View</p>')
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {'pets':pets})
 
 def pet_detail(request, id):
-    return HttpResponse('<p>Pet Detail View with the ID {}</p'.format(id))
+    try:
+        pet = Pet.objects.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet Not Found')
+    return render(request, 'pet_detail.html', {'pet':pet})
 
